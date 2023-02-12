533124448402dc7fd071b3ebcc4fd8aa6a99030b
bernardow <bernardoritzel@gmail.com>
2023-02-12 17:03:33 -0300

    js-added+stylized

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const inpName = document.getElementById('inpName');
 const inpTel = document.getElementById('inpTel');
 const btnAdd = document.getElementById('btnAddCtt');
 const form = document.getElementById('formCtt');
+const errorMessage = document.querySelector('#errorMessage');
 let cttNum = [];
+let cttTel = [];
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
@@ -11,6 +13,10 @@ form.addEventListener('submit', function(e){
     CalcContacts();
 
     ClearFields();
+})
+
+inpTel.addEventListener('change', function(e){
+    CheckDuplicates();
 })
 
 function ClearFields(){
@@ -30,7 +36,18 @@ function AddContact(){
 
 function CalcContacts(){
     cttNum.push(inpName.value);
+    cttTel.push(inpTel.value);
 
     const tfoot = document.querySelector('tfoot');
     tfoot.innerHTML = `<tr><td>Você tem ${cttNum.length} contatos</td><td></td></tr>`;
+}
+
+function CheckDuplicates(){
+    if(cttTel.includes(inpTel.value)){
+        btnAdd.disabled = true;
+        errorMessage.style.display = 'block';
+    }else {
+        btnAdd.disabled = false;
+        errorMessage.style.display = 'none';
+    }
 }
